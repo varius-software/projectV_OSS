85698d099426cac94021100e8987384f31db6769
NknightA <n.knight.pc0627@gmail.com>
2023-03-20 13:56:15 +0900

    first commmit.

---
+++ server-src/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Index () {
+    return(
+        <div>
+            <h1>Welcome to V!</h1>
+        </div>
+    )
+}
---
+++ server-src/src/server/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import next from "next";
+
+const dev = process.env.NODE_ENV === "development";
+const port = 4100;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+(async () => {
+    try {
+        await app.prepare();
+        const server = express();
+        server.all("*", (req: Request, res: Response) => {
+            return handle(req, res);
+        });
+        server.listen(port, () => {
+            console.log(`${port}で起動中`);
+        });
+    } catch (e) {
+        console.error(e);
+    }
+})();
