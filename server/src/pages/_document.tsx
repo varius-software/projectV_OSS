9a05c63f5deef8c6546f7c3988843d7facf30bab
NknightA <n.knight.pc0627@gmail.com>
2023-03-22 23:32:39 +0900

    update.

--- server/src/pages/_document.tsx
+++ server/src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+		<div>
+			<h1>VARIUS App</h1>
+			<p>App mode : {process.env.NODE_ENV}</p>
+		</div>
+	  </Head>
       <body>
         <Main />
         <NextScript />
