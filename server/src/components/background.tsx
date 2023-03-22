4299947689dacb451ce3d31c0c2ea6145e644559
NknightA <n.knight.pc0627@gmail.com>
2023-03-22 13:01:54 +0900

    fix src directory error.

---
+++ server/src/components/background.tsx
@@ -0,0 +1,9 @@
+import styles from "./background.module.sass"
+
+export default function Background () {
+	return (
+		<div className={styles.Main}>
+			<h1>Awesome METAVERSE...</h1>
+		</div>
+	)
+}
