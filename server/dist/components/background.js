3d6934ece3c932a823d57c986a6177820c87317c
NknightA <n.knight.pc0627@gmail.com>
2023-03-30 07:54:22 +0900

    webpacks

---
+++ server/dist/components/background.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var background_module_sass_1 = require("./background.module.sass");
+function Background() {
+    return (<div className={background_module_sass_1.default.Main}>
+			<h1>Awesome METAVERSE...</h1>
+		</div>);
+}
+exports.default = Background;
--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,38 +1,62 @@
-const path = require('path')
-const webpack = require('webpack')
-const nodeExternals = require('webpack-node-externals')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-module.exports = {
-    entry: {
-        server: './dist/server/server.js',
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        publicPath: '/dist',
-        filename: '[name].js'
-    },
-    target: 'node',
-    node: {
-        // Need this when working with express, otherwise the build fails
-        __dirname: false,   // if you don't put this is, __dirname
-        __filename: false,  // and __filename return blank or /
-    },
-    externals: [nodeExternals()], // Need this to avoid error when working with Express
-    module: {
-        rules: [
-            {
-                // Loads the javacript into html template provided.
-                // Entry point is set below in HtmlWebPackPlugin in Plugins
-                test: /\.html$/,
-                use: [{loader: "html-loader"}]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./index.html",
-            filename: "./index.html",
-            excludeChunks: [ 'server' ]
-        })
-    ]
-}
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+const config = {
+  entry: "./src/server/server.ts",
+  output: {
+    path: path.resolve(__dirname, "pud"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+
+    new MiniCssExtractPlugin(),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
