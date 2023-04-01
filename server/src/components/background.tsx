7d8234ded8171d5c7b6ddbdc754de2898c4dcb75
NknightA <n.knight.pc0627@gmail.com>
2023-04-02 00:44:49 +0900

    update

--- server/src/components/background.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./background.module.sass"
-
-export default function Background () {
-	return (
-		<div className={styles.Main}>
-			<h1>Awesome METAVERSE...</h1>
-		</div>
-	)
-}
--- server/src/components/mode.ts
+++
@@ -1,9 +0,0 @@
-export const devmode = () => {
-    let dev = 'dev';
-    let pud = 'pud';
-    if (process.env.NODE_ENV === "development") {
-        return dev
-    } else {
-        return pud
-    }
-}
