f2db5ca49f4344bf956de9d8410e5825760fd83e
NknightA <n.knight.pc0627@gmail.com>
2023-04-02 17:51:06 +0900

    update

---
+++ server/src/server/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import next from "next";
+const dev = process.env.NODE_ENV === "production";
+const port = "https://variussoftware.com";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+(async () => {
+    try {
+        await app.prepare();
+        const server = express();
+        server.all("*", (req: Request, res: Response) => {
+            return handle(req, res);
+        });
+        server.listen(port, () => {
+            console.log(`running https://variussoftware.com`);
+            //cgiExpress.execute(server);
+        });
+    } catch (e) {
+        console.error(e);
+    }
+})();
