61fb1ea15b7f4cba9ec872756bb4bdfb09a8b51c
NknightA <n.knight.pc0627@gmail.com>
2023-04-02 17:55:24 +0900

    udpate

---
+++ server/dist/pages/_app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("@/styles/globals.css");
+function App(_a) {
+    var Component = _a.Component, pageProps = _a.pageProps;
+    return <Component {...pageProps}/>;
+}
+exports.default = App;
