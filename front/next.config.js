205fc0ecf3fa36ff6a2e33f4970ca3ecc41ea5ef
NknightA <n.knight.pc0627@gmail.com>
2023-04-24 08:42:24 +0900

    fix now

--- front/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-/*const nextConfig = {
-  reactStrictMode: true,
-	...import(`./config/${process.env.APP_ENV || 'local'}`.json)
-}
-
-module.exports = nextConfig*/
